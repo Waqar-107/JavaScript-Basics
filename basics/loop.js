773d610ddfa89e24f3ac3e7616a5868c64bbae0d
Waqar-107 <waqar.hassan866@gmail.com>
2021-04-06 09:51:31 +0600

    added new codes

---
+++ basics/loop.js
@@ -0,0 +1,16 @@
+let a = 14;
+
+switch (a) {
+	case 10:
+		console.log('ten');
+		break;
+	case 11:
+		console.log('eleven');
+		break;
+	case 12:
+		console.log('twelve');
+		break;
+	default:
+		console.log('error');
+		break;
+}
--- basics/operators.js
+++ basics/operators.js
@@ -6,3 +6,12 @@ console.log(a == b);
 
 // this will give false
 console.log(a === b);
+
+// 1011
+let val = -11;
+
+// signed right shift
+console.log(val >> 2);
+
+// unsigned right shift
+console.log(val >>> 2);
